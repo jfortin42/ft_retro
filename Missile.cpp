7a3b60bdb5ccc3293c4d84d54842e27813f49563
jfortin42 <djo_68@hotmail.fr>
2016-12-20 18:30:01 +0100

    missile

--- Missile.cpp
+++ Missile.cpp
@@ -6,12 +6,30 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/13 17:20:14 by jfortin           #+#    #+#             */
-/*   Updated: 2016/12/13 17:48:20 by jfortin          ###   ########.fr       */
+/*   Updated: 2016/12/20 18:29:16 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Missile.hpp"
 
-Missile::Missile(unsigned int hp, unsigned int speed, std::string skin, AWeapon *weapon, t_coord coord)
-	: AEntity(hp, speed, skin, weapon, coord)
+Missile::Missile(unsigned int hp, unsigned int speed, std::string skin, AWeapon *weapon, t_coord coord, AEntity const &shooter)
+	: AEntity(hp, speed, skin, weapon, coord), shooter(shooter)
 {}
+
+Missile::Missile(Missile const &src) : AEntity(src), shooter(src.shooter)
+{
+	*this = src;
+}
+
+Missile	&Missile::operator=(Missile const &rhs)
+{
+	if (this != &rhs)
+	{
+		hp = rhs.hp;
+		speed = rhs.speed;
+		skin = rhs.skin;
+		weapon = rhs.weapon;
+		coord = rhs.coord;
+	}
+	return (*this);
+}
--- Missile.hpp
+++ Missile.hpp
@@ -6,19 +6,28 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 19:15:44 by jfortin           #+#    #+#             */
-/*   Updated: 2016/12/13 17:46:24 by jfortin          ###   ########.fr       */
+/*   Updated: 2016/12/20 18:29:13 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AEntity.hpp"
+#include "AWeapon.hpp"
+
+class	AEntity;
+class	AWeapon;
 
 class	Missile : public AEntity{
 
 public:
-	Missile(unsigned int hp, unsigned int speed, std::string skin, AWeapon *weapon, t_coord coord);
+	Missile(unsigned int hp, unsigned int speed, std::string skin, AWeapon *weapon, t_coord coord, AEntity const &shooter);
+	Missile(Missile const &src);
+	~Missile();
+
+	Missile		&operator=(Missile const &rhs);
 
 private:
 	Missile();
 
+	AEntity	const &shooter;
 
 };
